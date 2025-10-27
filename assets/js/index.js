e546a190e277b0593ef582e0764655f6a6772914
HimaXPro <hikmahkhaliqputra@gmail.com>
2025-10-27 11:09:50 +0700

    new

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled'); // tambahkan kelas saat di-scroll
+    } else {
+        navbar.classList.remove('scrolled'); // hilangkan jika di atas lagi
+    }
+}); s
